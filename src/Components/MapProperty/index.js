25db315e2ea6d545172f31cda3c99200669ccc91
Harsh Shah <harshm.shah@it-ally-u63.svaapta.local>
2022-01-03 20:20:44 +0530

    revert changes of mapbox

--- src/Components/MapProperty/index.js
+++ src/Components/MapProperty/index.js
@@ -1,7 +1,10 @@
-import React, { useRef, useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 // import { withScriptjs, withGoogleMap } from "react-google-maps";
 // import { DrawingManager } from "react-google-maps/lib/components/drawing/DrawingManager";
 // import { MarkerWithLabel } from "react-google-maps/lib/components/addons/MarkerWithLabel";
+import image17 from "../../assets/images/17.png";
+import markerIcon from "../../assets/images/marker-icon.png";
+import { Link } from "react-router-dom";
 import {
   LoadScript,
   GoogleMap,
@@ -11,145 +14,272 @@ import {
 } from "@react-google-maps/api";
 import { connect } from "react-redux";
 import "./map.css";
-import image17 from "../../assets/images/17.png";
-import markerIcon from "../../assets/images/marker-icon.png";
-import { Link } from "react-router-dom";
-import mapboxgl, { MatGeocoder, MapboxCircle, TouchPitchHandler } from 'mapbox-gl';
-import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
-class MapProperty extends React.PureComponent {
-  constructor(props) {
-    super(props);
+const mapContainerStyle = {
+  height: "120%",
+  width: "100%",
+};
 
-    console.log('props', this.props);
+const position = {
+  lat: 37.772,
+  lng: -122.214,
+};
 
-    this.state = {
-      lng: 40.20,
-      lat: -74.73,
-      zoom: 8,
-      PropertyList: this.props.propertiesList,
-      Property_coordinates: this.props.propertiesList.coordinates
-    };
-    this.mapContainer = React.createRef();
-    this.mapRecords = [];
-  }
-  
+const divStyle = {
+  background: `white`,
+  border: `1px solid #ccc`,
+  padding: 15,
+};
 
-  componentDidMount() {
+const libraries = ["drawing"];
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoiZWhvbWVvZmZlcm1hcHMiLCJhIjoiY2tqaW5mZGp6N282OTMycngzMjVveTR1byJ9.GO2cn4EGL2Q-78KiKa-z9g';
-    const maps = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lat,this.state.lng],
-      zoom: 6,
+const styles = {
+  padZero: {
+    padding: "0",
+  },
+  infoWindow: {
+    padding: "20px",
+    color: "#fff",
+    position: "relative",
+  },
+};
+
+function MapProperty(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState("");
+  const [options, setOptions] = useState({
+    drawingControl: true,
+    drawingControlOptions: {
+      drawingModes: ["polygon"],
+    },
+    polygonOptions: {
+      fillColor: `#2196F3`,
+      strokeColor: `#2196F3`,
+      fillOpacity: 0.5,
+      strokeWeight: 2,
+      clickable: false,
+      editable: false,
+      draggable: false,
+      zIndex: 1,
+      // visible:false
+    },
+  });
+
+  const divStyle = {
+    background: `white`,
+    border: `1px solid #ccc`,
+    padding: 15,
+  };
+
+  const onPolygonComplete = (polygon) => {
+    const polyArray = polygon.getPath().getArray();
+    const isvisible = polygon.getVisible();
+    let paths = [];
+    let location = [];
+    polyArray.forEach(function (path) {
+      paths.push([path.lng(), path.lat()]);
     });
-  
-    const styles = {
-      padZero: {
-        padding: "0",
-      },
-      infoWindow: {
-        padding: "20px",
-        color: "#fff",
-        position: "relative",
-      },
+
+    // polyArray.forEach(function (path) {
+    //     paths.push([path.lat(),path.lng()]);
+    // });
+    console.log(paths, "check paths");
+    paths.push(paths[0]);
+
+    const data = {
+      type: "Polygon",
+      coordinates: [paths],
     };
 
-    
-    if (this.state.PropertyList) {
-  
-      const mapRecords = this.props.propertiesList.map((item, index) => {
-        return ({
-          'type': 'Feature',
-          "properties": {
-            "title": '',
-            "description": `
-            <div className='rightlet'>
-            <img src='${item.images ? item.images[0] : ''}' width="100%" no-repeat;/>
-            </div>
-            <div>
-            <span>House for Sale</span> </br>
-            <span> ${item.address.street ? item.address.street : ''} </span>
-            </br> <span>$ ${item.listPrice}</span>
-            </br><span>Est. $321/mo</span></div>`},
-          "geometry": {
-            "coordinates": [item.coordinates.longitude, item.coordinates.latitude],
-            "type": "Point"
-          },
-        })
-      });      
-    
-      maps.on('load', () => {
-      
-        maps.addSource('places', {
-          type: 'geojson',
-          data: {
-            type: "FeatureCollection",
-            features: mapRecords
-          },
-        });
-        // Add a layer showing the places.
-        maps.addLayer({
-          'id': 'places',
-          'type': 'circle',
-          'source': 'places',
-          'paint': {
-            'circle-color': 'red',
-            'circle-radius': 6,
-            'circle-stroke-width': 2,
-            'circle-stroke-color': '#ffffff'
-          }
-        });
-    
-        // Create a popup, but don't add it to the map yet.
-        const popup = new mapboxgl.Popup({
-          closeButton: false,
-          closeOnClick: false
-        });
-    
-        maps.on('mouseenter', 'places', (e) => {
-          // Change the cursor style as a UI indicator.
-          maps.getCanvas().style.cursor = 'pointer';
-    
-          // Copy coordinates array.
-          const coordinates = e.features[0].geometry.coordinates.slice();
-          const description = e.features[0].properties.description;
-    
-          // Ensure that if the maps is zoomed out such that multiple
-          // copies of the feature are visible, the popup appears
-          // over the copy being pointed to.
-          while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-          }
-    
-          // Populate the popup and set its coordinates
-          // based on the feature found.
-          popup.setLngLat(coordinates).setHTML(description).addTo(maps);
-        });
-    
-        maps.on('mouseleave', 'places', () => {
-          maps.getCanvas().style.cursor = '';
-          popup.remove();
-        });
-      });
+    if (props.loadFromMap) {
+      props.loadFromMap(data);
     }
+    // props.fetchByMap({ data });
+    polygon.setMap(null);
+  };
 
+  const onMarkerClick = (item, index) => {
+    setIsOpen(true);
+    setSelectedIndex(index);
+  };
+  useEffect(() => {
+    if (props.myProperties.listings) {
+      if (props.setDataFromMap) {
+        props.setDataFromMap(props.myProperties.listings);
+      }
+    }
+  }, [props.myProperties]);
 
-  }
-  render() {
-    const { lng, lat, zoom } = this.state;
-    return (
-      <div ref={el => this.mapContainer = el} className="map-container" />
-    );
-  }
+  const center = props.coordinates
+    ? {
+      lat: props.coordinates.latitude,
+      lng: props.coordinates.longitude,
+    }
+    : {
+      lat: 40.675641937661645,
+      lng: -74.40732978647165,
+    };
+
+  return (
+    <LoadScript
+      libraries={libraries}
+      googleMapsApiKey="AIzaSyCNkbaUQ49AeNfiisnATf1UlrDixfyPk40"
+    >
+      <GoogleMap
+        id="drawing-manager-example"
+        height="100%"
+        className="h-100"
+        style={{ height: "100%" }}
+        mapContainerStyle={mapContainerStyle}
+        zoom={props.coordinates ? 18 : 9}
+        center={center}
+      >
+        {props.propertiesList && (
+          <>
+            {props.propertiesList.map((item, index, z) => {
+              return (
+                <>
+                  {item.address.state === "NJ" ? (
+                    <>
+                    {/* {console.info('item---NJ',item.address.state)}/ */}
+                      {item.coordinates && (
+                        <>
+                          {item.coordinates.coordinates ? (
+                            <Marker
+                              key={index}
+                              onClick={() => onMarkerClick(item, index)}
+                              title={item.address.street}
+                              id={item.id}
+                              icon={{
+                                url: markerIcon,
+                              }}
+                              position={{
+                                lat: item.coordinates.coordinates[1],
+                                lng: item.coordinates.coordinates[0],
+                              }}
+                            >
+                              {selectedIndex === index &&
+                                !props.coordinates(
+                                  <InfoWindow style={styles.padZero}>
+                                    <div
+                                      style={{
+                                        background: `url(${item.images[0]}) no-repeat`,
+                                        ...styles.infoWindow,
+                                      }}
+                                    >
+                                      <div className="info-content">
+                                        <span>House for Sale</span>
+                                        <div className="left-content">
+                                          <Link
+                                            to={{
+                                              pathname: "/property-view",
+                                              state: { id: item.id },
+                                            }}
+                                            className="name"
+                                          >
+                                            {item.address.street}
+                                          </Link>
+                                          <span>${item.listPrice}</span>
+                                          <span>Est. $321/mo</span>
+                                        </div>
+                                        <div className="right-content">
+                                          <img
+                                            // onClick={addFavourite}
+                                            src={image17}
+                                            alt=""
+                                          />
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </InfoWindow>
+                                )}
+                            </Marker>
+                          ) : (
+                            <Marker
+                              key={index}
+                              onClick={() => onMarkerClick(item, index)}
+                              title={item.address.street}
+                              id={item.id}
+                              icon={{
+                                url: markerIcon,
+                              }}
+                              position={{
+                                lat: item.coordinates.latitude,
+                                lng: item.coordinates.longitude,
+                              }}
+                            >
+                              {/* on click of marker we getting one popup related perticular property and we can see some details on popup */}
+                              {selectedIndex === index && (
+                                <InfoWindow className="p-0">
+                                  <div
+                                    className="info-Window"
+                                    style={{
+                                      background: `url(${item.images[0]}) no-repeat`,
+                                    }}
+                                  >
+                                    <div className="info-content">
+                                      <span>House for Sale</span>
+                                      <div className="left-content">
+                                        <Link
+                                          to={{
+                                            pathname: "/property-view",
+                                            state: { id: item.id },
+                                          }}
+                                          className="name"
+                                        >
+                                          {item.address.street}
+                                        </Link>
+                                        <span>${item.listPrice}</span>
+                                        <span>Est. $321/mo</span>
+                                      </div>
+                                      <div className="right-content">
+                                        <img
+                                          // onClick={addFavourite}
+                                          src={image17}
+                                          alt=""
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+                                </InfoWindow>
+                              )}
+                            </Marker>
+                          )}
+                        </>
+                      )}
+                    </>
+
+                  ) : ''}
+                </>
+              );
+            })}
+          </>
+        )}
+
+        {/* as we create shape of polygon */}
+        <DrawingManager
+          drawingMode="polygon"
+          options={options}
+          onPolygonComplete={onPolygonComplete}
+        // overlaycomplete={(poly) => {
+        //     const polyArray = poly.getPath().getArray();
+        //     let paths = [];
+        //     polyArray.forEach(function (path) {
+        //         paths.push({
+        //             longitude: path.lng(),
+        //             latitude: path.lat()
+        //         });
+        //     });
+        // }}
+        />
+      </GoogleMap>
+    </LoadScript>
+  );
 }
-
 const mapStateToProps = (state) => {
   return {
     myProperties: state.myProperties,
     mainLoader: state.loader,
   };
 };
-
 export default connect(mapStateToProps)(MapProperty);
